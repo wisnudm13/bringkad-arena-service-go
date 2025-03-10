17296a3e4deea4a71a65a4d0ec81435a2d4afa75
wisnudm13 <wisnudentadio@gmail.com>
2025-03-10 15:12:03 +0700

    feat: add env config, db connecton, live reload

---
+++ databases/db.go
@@ -0,0 +1,31 @@
+package databases
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbHost, dbUser, dbPassword, dbName, dbPort)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Error connecting to DB:", err)
+	}
+
+	DB = db
+
+}
--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+
+	"bringkad-arena-service-go/config"
+	"bringkad-arena-service-go/databases"
+	v1 "bringkad-arena-service-go/routes/v1"
+
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterRouters(r *gin.Engine) {
-	// register v1 routes
-}
-
 func main() {
+	// Init gin router
 	router := gin.Default()
-	RegisterRouters(router)
+
+	// Load ENV
+	config.LoadENV()
+
+	// Connect to DB
+	databases.ConnectDB()
+
+	// Health check API
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "All system running successfully"})
+	})
+
+	// Register API routes
+	apiGroup := router.Group("/api")
+	v1.RegisterRoutes(apiGroup.Group("v1"))
+
+	// Start server
+	port := os.Getenv("APP_PORT")
+	if err := router.Run(":" + port); err != nil {
+		log.Fatalf("Failed to start system at port %v, error: %v", port, err)
+	}
+
 }
